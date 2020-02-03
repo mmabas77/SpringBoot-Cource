a8d2b9d2e7f9a7de392d14461c6c7834478dfd1e
Mahmoud Abas <mmabas77@gmail.com>
2020-02-04 00:29:38 +0200

    Initial Commit For SpringBoot Project

---
+++ src/main/java/com/mmabas77/SpringBootCource0Application.java
@@ -0,0 +1,13 @@
+package com.mmabas77;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCource0Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCource0Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/mmabas77/SpringBootCource0ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mmabas77;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCource0ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
