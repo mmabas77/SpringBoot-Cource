95e50d30328fbf4892e9ae43069cdfb61d7250a3
Mahmoud Abas <mmabas77@gmail.com>
2020-04-15 13:58:29 +0200

    Added Logging Capability

--- src/main/java/com/mmabas77/web/i18n/I18NService.java
+++ src/main/java/com/mmabas77/web/i18n/I18NService.java
@@ -1,5 +1,7 @@
 package com.mmabas77.web.i18n;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
@@ -10,10 +12,14 @@ import java.util.Locale;
 @Service
 public class I18NService {
 
+    /* The Application Logger*/
+    private static final Logger LOG =  LoggerFactory.getLogger(I18NService.class);
+
     @Autowired
     private MessageSource messageSource;
 
     public String getMessage(String msgId) {
+        LOG.info("Returning i18n text for messageId : {}",msgId);
         Locale locale = LocaleContextHolder.getLocale();
         return getMessage(msgId, locale);
     }
